9165ac9c48bf12e5594e232c33d3dcfeb4f4bdd3
franek <franciszek.slupski@supermemo.com>
2019-04-13 19:01:49 +0200

    init rest controllers configuration

---
+++ src/main/java/com/slupski/hello/HelloController.java
@@ -0,0 +1,14 @@
+package com.slupski.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        System.out.println("Hello");
+        return "com/slupski/hello";
+    }
+}
